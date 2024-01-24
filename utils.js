501482ed4db535d27a52a82a721af804e70dbccb
PierreGoupil13 <p.goupil356@gmail.com>
2024-01-24 08:18:36 +0100

    Push pour cours

--- utils.js
+++ utils.js
@@ -1,3 +1,13 @@
 function lerp(A, B, t) {
     return A + (B - A) * t;
 }
+
+/*
+function getIntersection(pt1, pt2, pt3, pt4) {
+
+    return {
+        x: ,
+        y: ,
+        offset:
+    }
+}*/
