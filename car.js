6843921a96e8a1bc66a95778a73599f64a94e7fe
pierregoupil <p.goupil356@gmail.com>
2024-02-05 14:26:15 +0100

    Récupération après perte données

--- car.js
+++ car.js
@@ -1,84 +1,128 @@
-class Car {
-    constructor(x,y,width,height, canvas) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.canva = canvas;
+class Car{
+    constructor(x,y,width,height,controlType,maxSpeed=3){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
 
-        this.speed = 0;
-        this.acceleration = 0.2;
-        this.maxSpeed = 3;
-        this.friction = 0.05;
-        this.angle = 0;
+        this.speed=0;
+        this.acceleration=0.2;
+        this.maxSpeed=maxSpeed;
+        this.friction=0.05;
+        this.angle=0;
+        this.damaged=false;
 
-        this.sensor = new Sensor(this);
-        this.controls = new Controls();
+        if(controlType!="DUMMY"){
+            this.sensor=new Sensor(this);
+        }
+        this.controls=new Controls(controlType);
     }
 
-    update(roadBorders) {
-        this.#move();
-        this.sensor.update(roadBorders);
+    update(roadBorders,traffic){
+        if(!this.damaged){
+            this.#move();
+            this.polygon=this.#createPolygon();
+            this.damaged=this.#assessDamage(roadBorders,traffic);
+        }
+        if(this.sensor){
+            this.sensor.update(roadBorders,traffic);
+        }
     }
 
-    #move() {
-        if (this.controls.forward) {
-            this.speed += this.acceleration;
-        } else if (this.controls.reverse) {
-            this.speed -= this.acceleration;
+    #assessDamage(roadBorders,traffic){
+        for(let i=0;i<roadBorders.length;i++){
+            if(polysIntersect(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+        for(let i=0;i<traffic.length;i++){
+            if(polysIntersect(this.polygon,traffic[i].polygon)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    #createPolygon(){
+        const points=[];
+        const rad=Math.hypot(this.width,this.height)/2;
+        const alpha=Math.atan2(this.width,this.height);
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(this.angle+alpha)*rad,
+            y:this.y-Math.cos(this.angle+alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+        });
+        return points;
+    }
+
+    #move(){
+        if(this.controls.forward){
+            this.speed+=this.acceleration;
+        }
+        if(this.controls.reverse){
+            this.speed-=this.acceleration;
         }
 
-        if (this.speed > this.maxSpeed) {
-            this.speed = this.maxSpeed;
+        if(this.speed>this.maxSpeed){
+            this.speed=this.maxSpeed;
+        }
+        if(this.speed<-this.maxSpeed/2){
+            this.speed=-this.maxSpeed/2;
         }
 
-        if (this.speed < -this.maxSpeed / 2) {
-            this.speed = -this.maxSpeed / 2;
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0;
         }
 
-        if (this.speed > 0) {
-            this.speed -= this.friction;
-        }
-
-        if (this.speed < 0) {
-            this.speed += this.friction;
-        }
-
-        if (Math.abs(this.speed) < this.friction) {
-            this.speed = 0;
-        }
-
-        if(this.speed !== 0) {
-            const flip = this.speed > 0 ? 1 : -1;
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
             // C'est une sorte de cercle de trigo mais inversé, donc - pour la droite et + pour la gauche
             // borne droite -pi/2 borne gauche cest +pi/2
-            if (this.controls.right) {
-                this.angle -= 0.03 * flip;
+
+            if(this.controls.left){
+                this.angle+=0.03*flip;
             }
-            if (this.controls.left) {
-                this.angle+= 0.03 * flip;
+            if(this.controls.right){
+                this.angle-=0.03*flip;
             }
         }
 
-        this.x -= Math.sin(this.angle) * this.speed;
-        this.y -= Math.cos(this.angle) * this.speed;
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw() {
-        ctx.save()
-        ctx.translate(this.x, this.y);
-        ctx.rotate(-this.angle);
-
-        ctx.beginPath()
-        ctx.fillRect(
-            - this.width / 2,
-            - this.height / 2,
-            this.width, this.height);
+    draw(ctx,color){
+        if(this.damaged){
+            ctx.fillStyle="gray";
+        }else{
+            ctx.fillStyle=color;
+        }
+        ctx.beginPath();
+        ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
+        for(let i=1;i<this.polygon.length;i++){
+            ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
+        }
         ctx.fill();
 
-        ctx.restore();
-
-        this.sensor.draw(ctx);
+        if(this.sensor){
+            this.sensor.draw(ctx);
+        }
     }
-
 }
--- controls.js
+++ controls.js
@@ -1,46 +1,52 @@
-class Controls {
-    constructor() {
-        this.forward = false;
-        this.left = false;
-        this.right = false;
-        this.reverse = false;
+class Controls{
+    constructor(type){
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
 
-        this.#addKeyboardListeners();
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward=true;
+                break;
+        }
     }
 
-    #addKeyboardListeners() {
-        document.addEventListener("keydown", (evt) => {
-            switch(evt.key) {
-                case "ArrowUp" :
-                    this.forward = true;
+    #addKeyboardListeners(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=true;
                     break;
-                case "ArrowLeft" :
-                    this.left = true;
+                case "ArrowRight":
+                    this.right=true;
                     break;
-                case "ArrowRight" :
-                    this.right = true;
+                case "ArrowUp":
+                    this.forward=true;
                     break;
-                case "ArrowDown" :
-                    this.reverse = true;
-                    break;
-            }
-        });
-        document.addEventListener("keyup", (evt) => {
-            switch(evt.key) {
-                case "ArrowUp" :
-                    this.forward = false;
-                    break;
-                case "ArrowLeft" :
-                    this.left = false;
-                    break;
-                case "ArrowRight" :
-                    this.right = false;
-                    break;
-                case "ArrowDown" :
-                    this.reverse = false;
+                case "ArrowDown":
+                    this.reverse=true;
                     break;
             }
-
-        })
+        }
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=false;
+                    break;
+                case "ArrowRight":
+                    this.right=false;
+                    break;
+                case "ArrowUp":
+                    this.forward=false;
+                    break;
+                case "ArrowDown":
+                    this.reverse=false;
+                    break;
+            }
+        }
     }
 }
