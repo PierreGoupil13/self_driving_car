b331a4ea644171f60b106f75bb85fda50c6d96b7
Pierre Goupil <pierre.goupil@makeitsafe.fr>
2024-02-13 14:56:32 +0100

    Fin du projet de cours, début fine tunning

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+/*
+    Idées :
+    - Faire de meilleurs fonction de fine tunning,
+    - Implémenter des algos de genetic comme du cross over
+ */
+
 const carCanvas=document.getElementById("carCanvas");
 carCanvas.width=200;
 
@@ -8,33 +14,82 @@ const carCTX = carCanvas.getContext("2d");
 const networkCTX = networkCanvas.getContext("2d");
 
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
-const car=new Car(road.getLaneCenter(1),100,30,50,"AI");
+const N = 1000;
+const cars= generateCars(N)
+let bestCar = cars[0];
+
+if(localStorage.getItem("bestBrain")) {
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].brain = JSON.parse(
+            localStorage.getItem("bestBrain")
+        );
+        if(i !== 0) {
+            NeuralNetwork.mutate(cars[i].brain, 0.1)
+        }
+    }
+}
+
 const traffic=[
-    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2)
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2, getRandomColor()),
+    new Car(road.getLaneCenter(2),-400,30,50,"DUMMY",2, getRandomColor()),
+    new Car(road.getLaneCenter(1),-600,30,50,"DUMMY",2, getRandomColor()),
+    new Car(road.getLaneCenter(2),-100,30,50,"DUMMY",2, getRandomColor())
 ];
 
 animate();
 
+function save(){
+    localStorage.setItem("bestBrain",
+        JSON.stringify(bestCar.brain));
+}
+
+function discard(){
+    localStorage.removeItem("bestBrain");
+}
+function generateCars(N){
+    const cars = [];
+    for (let i = 1; i<= N; i++) {
+        cars.push(new Car(road.getLaneCenter(1), 100, 30,50, "AI", 3, getRandomColor()));
+    }
+    return cars;
+}
 function animate(){
     for(let i=0;i<traffic.length;i++){
         traffic[i].update(road.borders,[]);
     }
-    car.update(road.borders,traffic);
+    for (let i=0; i<cars.length; i++){
+        cars[i].update(road.borders,traffic);
+    }
+
+    // Homework intéressant serait de créer plusieurs Fitness Function
+    bestCar = cars.find(
+        c => c.y === Math.min(
+            ...cars.map(
+                c => c.y
+            ))
+    );
 
     carCanvas.height=window.innerHeight;
     networkCanvas.height=window.innerHeight;
 
     carCTX.save();
-    carCTX.translate(0,-car.y+carCanvas.height*0.7);
+    carCTX.translate(0,-bestCar.y+carCanvas.height*0.7);
 
     road.draw(carCTX);
     for(let i=0;i<traffic.length;i++){
         traffic[i].draw(carCTX,"red");
     }
-    car.draw(carCTX,"blue");
+
+    carCTX.globalAlpha=0.2;
+    for (let i=0; i<cars.length; i++){
+        cars[i].draw(carCTX,"blue");
+    }
+    carCTX.globalAlpha = 1;
+    bestCar.draw(carCTX, "blue", true);
 
     carCTX.restore();
 
-    Visualizer.drawNetwork(networkCTX, car.brain)
+    Visualizer.drawNetwork(networkCTX, bestCar.brain)
     requestAnimationFrame(animate);
 }
